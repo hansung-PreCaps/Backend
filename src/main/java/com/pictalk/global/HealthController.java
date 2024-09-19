7cd75986e8272384b56ed7aab472c7775b287b60
enble <enble_777@naver.com>
2024-09-19 23:01:35 +0900

    chore: swagger settings

---
+++ src/main/java/com/pictalk/global/HealthController.java
@@ -0,0 +1,24 @@
+package com.pictalk.global;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "상태 확인", description = "서버의 상태가 정상적인지 확인합니다.")
+@RestController
+public class HealthController {
+
+    @Operation(summary = "서버 상태 확인", description = "서버의 상태가 정상적인지 확인합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "서버 상태가 정상입니다."),
+            @ApiResponse(responseCode = "500", description = "서버 상태가 비정상입니다.")
+    })
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+
+}
