629de677115b58712bf6669e3f5ed622ee6244dd
enble <enble_777@naver.com>
2024-09-27 22:25:36 +0900

    feat: 공통 응답 및 공통 예외 처리

---
+++ src/main/java/com/pictalk/global/payload/status/ErrorStatus.java
@@ -0,0 +1,40 @@
+package com.pictalk.global.payload.status;
+
+import com.pictalk.global.payload.ReasonDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorStatus implements BaseStatus {
+    // Common Error
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_500", "서버 에러입니다. 관리자에게 문의하세요."),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON_400", "잘못된 요청입니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON_401", "인증이 필요합니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON_403", "금지된 요청입니다."),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ReasonDto getReason() {
+        return ReasonDto.builder()
+                .message(message)
+                .code(code)
+                .isSuccess(false)
+                .build();
+    }
+
+    @Override
+    public ReasonDto getReasonHttpStatus() {
+        return ReasonDto.builder()
+                .status(httpStatus)
+                .message(message)
+                .code(code)
+                .isSuccess(false)
+                .build();
+    }
+}
