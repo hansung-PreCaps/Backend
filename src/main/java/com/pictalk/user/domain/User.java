64966caf971d171bc8a0bd7ab9fbf4a1d90eba16
sanghun <vitash1215@gmail.com>
2024-10-29 16:16:32 +0900

    fix : user, sender 연관관계 설정

--- src/main/java/com/pictalk/user/domain/User.java
+++ src/main/java/com/pictalk/user/domain/User.java
@@ -1,14 +1,17 @@
 package com.pictalk.user.domain;
 
 import com.pictalk.global.common.BaseEntity;
+import com.pictalk.message.domain.Sender;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import java.util.ArrayList;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import java.util.List;
 
 
 @Entity
@@ -23,6 +26,9 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id", nullable = false)
     private Long id;
+
+    @OneToMany(mappedBy = "user")
+    private List<Sender> senders = new ArrayList<>();
 
     @NotEmpty(message = "Username is required")
     private String username;
