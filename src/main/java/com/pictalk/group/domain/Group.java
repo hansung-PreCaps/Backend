f90d2a1449cee8361c6a4deb32a7952a1896094a
sanghun <vitash1215@gmail.com>
2024-11-15 17:15:30 +0900

    fix : extend baseEntity

--- src/main/java/com/pictalk/group/domain/Group.java
+++ src/main/java/com/pictalk/group/domain/Group.java
@@ -1,20 +1,30 @@
 package com.pictalk.group.domain;
 
+import com.pictalk.global.common.BaseEntity;
 import com.pictalk.user.domain.User;
-import jakarta.persistence.*;
-import lombok.*;
-
-import java.time.LocalDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "`groups`")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
-public class Group {
+public class Group extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,24 +36,18 @@ public class Group {
     private User user;
 
     @OneToMany(mappedBy = "group")
-    @Builder.Default
     private List<GroupReceiver> groupReceivers = new ArrayList<>();
 
     private String name;
 
-    @Column(nullable = false)
-    private LocalDateTime createdAt;
-
-    @Builder.Default
-    private boolean isDeleted = false;
-
-    @PrePersist
-    protected void onCreate() {
-        createdAt = LocalDateTime.now();
-    }
-
+    @Builder
     public Group(User user, String name) {
         this.user = user;
         this.name = name;
     }
+
+    public void addGroupReceiver(GroupReceiver groupReceiver) {
+        groupReceivers.add(groupReceiver);
+    }
+
 }
