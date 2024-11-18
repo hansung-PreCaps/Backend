fc412146442a3094dd8602c51370bd0540e5a3ae
sanghun <vitash1215@gmail.com>
2024-11-18 14:21:17 +0900

    fix : 불필요 필드 삭제

--- src/main/java/com/pictalk/message/domain/Sender.java
+++ src/main/java/com/pictalk/message/domain/Sender.java
@@ -1,12 +1,25 @@
 package com.pictalk.message.domain;
 
 import com.pictalk.user.domain.User;
-import jakarta.persistence.*;
-import lombok.*;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "sender")
@@ -36,9 +49,6 @@ public class Sender {
 
     @Column(updatable = false)
     private LocalDateTime createdAt;
-
-    @Builder.Default
-    private boolean deleted = false;
 
     @PrePersist
     protected void onCreate() {
